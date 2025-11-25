bb0db5fbafea7a7afe5a3906e7554fa331426317
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-11-26 01:35:49 +0500

    frontend backend connectivity

---
+++ backend/index.js
@@ -0,0 +1,68 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;  
+
+
+app.get('/', (req, res) => {
+    res.send("hello waqas")
+})
+
+const students = [
+  {
+    id: 1,
+    name: "Ali Raza",
+    age: 20,
+    email: "ali.raza@example.com",
+    department: "Computer Science",
+    grade: "A",
+    enrolled: true
+  },
+  {
+    id: 2,
+    name: "Sara Khan",
+    age: 22,
+    email: "sara.khan@example.com",
+    department: "Software Engineering",
+    grade: "B",
+    enrolled: true
+  },
+  {
+    id: 3,
+    name: "Waqas Ahmed",
+    age: 19,
+    email: "waqas.ahmed@example.com",
+    department: "IT",
+    grade: "A+",
+    enrolled: false
+  },
+  {
+    id: 4,
+    name: "Hamza Ali",
+    age: 21,
+    email: "hamza.ali@example.com",
+    department: "Computer Science",
+    grade: "C",
+    enrolled: true
+  },
+  {
+    id: 5,
+    name: "Ayesha Malik",
+    age: 23,
+    email: "ayesha.malik@example.com",
+    department: "AI & Data Science",
+    grade: "B+",
+    enrolled: true
+  }
+];
+
+app.get('/api/students', (req, res) => {
+    res.json(students)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+function App() {
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/students')
+    .then(response => {
+      setStudents(response.data)
+    })
+    .catch(error => {
+      console.error("There was an error fetching the students!", error)
+    })
+  },[])
+
+  return (
+    <>
+      <h1>M_waqas</h1>
+      <h4>Total students : {students.length}</h4>
+
+      <div>
+        {
+          students.map(student => (
+            <div key={student.id} style={{ border: '1px solid gray', margin: '10px', padding: '10px' }}>
+              <h3>Name: {student.name}</h3>
+              <p>Age: {student.age}</p>
+              <p>Email: {student.email}</p>
+              <p>Department: {student.department}</p>
+              <p>Grade: {student.grade}</p>
+              <p>Enrolled: {student.enrolled ? 'Yes' : 'No'}</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
